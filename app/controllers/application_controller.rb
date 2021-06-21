e2120e5cb730b599ad86aa92e5182370586c8a3a
Lena Hou <lenahou2013@gmail.com>
2021-06-21 17:11:18 -0400

    commit tree 2e0dfad3ae2429386ff1d8c985b1183da798a31b

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,47 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+     redirect '/articles'
   end
+
+  get '/articles' do
+    @articles = Article.all
+
+    erb :index
+  end
+
+  get '/articles/new' do
+    
+    erb :new
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+
+    erb :show
+  end
+
+
+  post '/articles' do
+    @article = Article.create(:title => params[:title], :content => params[:content])
+    redirect to "/articles/#{@article.id}"
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find_by(:id => params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find_by(:id => params[:id])
+    @article.update(params[:article])
+    redirect "/articles/#{@article.id}"
+  end
+
+  delete '/articles/:id' do
+    Article.destroy(params[:id])
+
+    redirect "/articles"
+  end
+
 end
